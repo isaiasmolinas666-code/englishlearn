177d082bc894d1749a5694cae882940c067cac6e
isaiasmolinas666-code <isaiasmolinas666@gmail.com>
2026-04-30 10:56:51 -0300

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -56,7 +56,7 @@ exports.handler = async function(event) {
         'X-Title': 'English Learning System',
       },
       body: JSON.stringify({
-       model: 'google/gemma-3-27b-it:free',
+       model: 'nousresearch/hermes-3-llama-3.1-405b:free',
         max_tokens: 300,
         messages: messages,
       }),
